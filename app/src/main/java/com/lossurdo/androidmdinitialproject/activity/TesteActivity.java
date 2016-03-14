ecff190367822e1a89b45b51ba641a9f3a8f0f9b
Rafael Lossurdo <lossurdo@gmail.com>
2016-03-14 17:34:24 -0300

    compatibilidade com google maps

--- app/src/main/java/com/lossurdo/androidmdinitialproject/activity/TesteActivity.java
+++ app/src/main/java/com/lossurdo/androidmdinitialproject/activity/TesteActivity.java
@@ -1,5 +1,6 @@
 package com.lossurdo.androidmdinitialproject.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +18,9 @@ public class TesteActivity extends AppCompatActivity {
     @Bind(R.id.btnDialog1)
     Button btnDialog1;
 
+    @Bind(R.id.btnMapa)
+    Button btnMapa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +33,14 @@ public class TesteActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Timber.d("Clicado no botão de Diálogo 1");
                 Toast.makeText(TesteActivity.this, "Clicado no botão de Diálogo 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Timber.d("Clicado no botão de MAPA");
+                startActivity(new Intent(getBaseContext(), MapaActivity.class));
             }
         });
     }
